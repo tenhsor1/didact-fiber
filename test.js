19d91141c0823da302e013138b38324a14bd0973
Ricardo Romo <ricardo.romo.ramirez@gmail.com>
2019-05-17 15:20:15 -0500

    Implented didact-fiber

---
+++ test.js
@@ -0,0 +1,68 @@
+/** @jsx Didact.createElement */
+import Didact from "./lib";
+const stories = [
+  {
+    name: "Didact introduction",
+    url: "http://bit.ly/2pX7HNn"
+  },
+  {
+    name: "Rendering DOM elements ",
+    url: "http://bit.ly/2qCOejH"
+  },
+  {
+    name: "Element creation and JSX",
+    url: "http://bit.ly/2qGbw8S"
+  },
+  {
+    name: "Instances and reconciliation",
+    url: "http://bit.ly/2q4A746"
+  },
+  {
+    name: "Components and state",
+    url: "http://bit.ly/2rE16nh"
+  }
+];
+
+class Story extends Didact.Component {
+  constructor(props) {
+    super(props);
+    this.state = { likes: Math.ceil(Math.random() * 100) };
+  }
+  like() {
+    this.setState({
+      likes: this.state.likes + 1
+    });
+  }
+  render() {
+    const { name, url } = this.props;
+    const { likes } = this.state;
+    return (
+      <li>
+        <button onClick={e => this.like()}>
+          {likes}
+          <b>❤</b>
+        </button>
+        <a href={url}>{name}</a>
+      </li>
+    );
+  }
+}
+
+class App extends Didact.Component {
+  render() {
+    return (
+      <div>
+        <h1>Didact Stories</h1>
+        <ul>
+          {this.props.stories.map(story => {
+            return <Story name={story.name} url={story.url} />;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  Didact.render(<App stories={stories} />, document.getElementById("root"));
+});
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+var webpack = require("webpack");
+module.exports = {
+  entry: "./test.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "app.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        query: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: "source-map",
+  mode: "development"
+};
